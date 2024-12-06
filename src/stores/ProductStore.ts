cf030aa8c567cb55ac51f482aaac9905aaef6fd1
Zaydrian Price <zaydrianp@gmail.com>
2024-12-05 20:52:12 -0500

    adds initProducts to firebase

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -1,6 +1,8 @@
 import { defineStore } from "pinia";
 import { ProductDoc } from "../types/product";
 import { initProducts } from "../data-init";
+import { db } from "../main.ts"
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 
 export const useProductStore = defineStore("ProductStore", {
@@ -8,8 +10,23 @@ export const useProductStore = defineStore("ProductStore", {
     products: [] as ProductDoc[],
   }),
   actions: {
-    init() {
-      this.products = initProducts;
+    async init() {
+      const productsCollection = collection(db, "products");
+      const querySnapshot = await getDocs(productsCollection);
+      if (querySnapshot.empty){
+        for (const product of initProducts) {
+          await addDoc(productsCollection, product.data);
+        }
+        const newQuery = await getDocs(productsCollection);
+        newQuery.forEach((doc) => {
+          this.products.push({ id: doc.id, data: doc.data() } as ProductDoc);
+        });
+      }
+      else{
+        querySnapshot.forEach((doc) => {
+          this.products.push({ id: doc.id, data: doc.data() } as ProductDoc);
+        });
+      }
     },
     filterByCategory(category: string) {
       return this.products.filter(product => product.data.category === category);
